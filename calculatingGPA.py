011165ad8203adc5db994015576ebb169eda24ff
Deji Obasan <dejiobasan@yahoo.com>
2022-10-26 08:47:01 +0100

    Initial Commit

---
+++ calculatingGPA.py
@@ -0,0 +1,75 @@
+#this program calculates GPA for a semester
+
+import os
+from math import *
+
+number_of_courses = float(input("Enter the number of courses offered this semester: "))
+
+number_of_units = float(input("Enter the total number of unit registered this semester: "))
+
+grades_c = {"A":5, "B":4, "C":3, "D":2, "E":1, "F":0 }
+
+course_code_1 = ""
+course_code_2 = ""
+course_code_3 = ""
+course_code_4 = ""
+course_code_5 = ""
+course_code_6 = ""
+course_code_7 = ""
+course_code_8 = ""
+course_code_9 = ""
+
+def Course_codes(code):
+    for course_code in Course_codes:
+        print(course_code)
+Course_codes = [
+    course_code_1 == input("Course Code1: "),
+    course_code_2 == input("Course Code2: "),
+    course_code_3 == input("Course Code3: "),
+    course_code_4 == input("Course Code4: "),
+    course_code_5 == input("Course Code5: "),
+    course_code_6 == input("Course Code6: "),
+    course_code_7 == input("Course Code7: "),
+    course_code_8 == input("Course Code8: "),
+    course_code_9 == input("Course Code9: "),
+]
+
+Grade_1 = ""
+Grade_2 = ""
+Grade_3 = ""
+Grade_4 = ""
+Grade_5 = ""
+Grade_6 = ""
+Grade_7 = ""
+Grade_8 = ""
+Grade_9 = ""
+
+def Course_grades(grade):
+    for course_grade in Course_grades:
+        print(course_grade)
+Course_grades = [
+    Grade_1 == str(print(course_code_1)) + (input("Grade_for_course_1: ")),
+    Grade_2 == str(print(course_code_2)) + (input("Grade_for_Course_2: ")),
+    Grade_3 == str(print(course_code_3)) + (input("Grade_for_Course_3: ")),
+    Grade_4 == str(print(course_code_4)) + (input("Grade_for_Course_4: ")),
+    Grade_5 == str(print(course_code_5)) + (input("Grade_for_Course_5: ")),
+    Grade_6 == str(print(course_code_6)) + (input("Grade_for_Course_6: ")),
+    Grade_7 == str(print(course_code_7)) + (input("Grade_for_Course_7: ")),
+    Grade_8 == str(print(course_code_8)) + (input("Grade_for_Course_8: ")),
+    Grade_9 == str(print(course_code_9)) + (input("Grade_for_Course_9: ")),
+]
+
+
+
+
+#def Calculating_GPA(Grades):
+
+
+
+
+
+
+
+
+
+
